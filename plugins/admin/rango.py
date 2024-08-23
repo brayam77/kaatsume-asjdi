01e935b3a45e361a3dca3d2f3d0b1b2ef0d3d22b
brayam77 <73500402+brayam77@users.noreply.github.com>
2024-08-23 17:54:37 -0400

    Update rango.py

--- plugins/admin/rango.py
+++ plugins/admin/rango.py
@@ -1,24 +1,30 @@
-
 from procesos.funciones import addCommand
 from procesos.mongo_data import MongoClient
-
 
 @addCommand('rango')
 def panel(_, message):    
     try:
         queryU = MongoClient().user_query(message.from_user.id)
-        if queryU == None: return message.reply('usa el comando para registrarse /register')
-        if queryU['rango'] == 'Baneado': return message.reply('usuario baneado no puede usarme.')
+        if queryU is None: 
+            return message.reply('Usa el comando para registrarse /register')
+        
+        if queryU['rango'] == 'Baneado': 
+            return message.reply('Usuario baneado no puede usarme.')
+        
+        # Verifica si el usuario tiene el rol de 'Owner' o 'Dev-Bol'
+        if queryU['rango'] not in ['Owner', 'Dev-Bol']:
+            return message.reply('No tienes permiso para usar este comando.')
     
         queryS = MongoClient().seller(message.from_user.id)
-        if queryS == False: return message.reply('Comando para admins')
+        if queryS == False: 
+            return message.reply('Comando para admins')
 
         data = message.text.split(' ')
-        if len(data) < 3: return message.reply('usalo /rango <id> <rango>')
+        if len(data) < 3: 
+            return message.reply('Úsalo /rango <id> <rango>')
 
         MongoClient().rango(int(data[1]), data[2])
-        message.reply(f'Se ha actualizado el rango de : {data[2]} Ahora es  {data[1]}')
-
+        message.reply(f'Se ha actualizado el rango de: {data[1]}. Ahora es {data[2]}')
     
-    except:     message.reply('No se encontro usuario.')
-    
+    except Exception as e:     
+        message.reply(f'No se encontró usuario. Error: {str(e)}')
